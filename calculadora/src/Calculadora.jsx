eae03a37f4fe48d47654b8ad7534e6aa54061231
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2022-02-17 23:48:55 -0300

    arquivo calculadora.css
    Nesse commit criei o arquivo calculadora.css

---
+++ calculadora/src/Calculadora.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Calculadora.css'
+
+
+export default function Calculadora() {
+    return (
+        <div>
+        <button>1</button>
+        </div>
+    )
+}
